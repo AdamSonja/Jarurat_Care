94f45fd77b1838c014c4a037fb5a576ea05bfcbd
Sunny K Sebu <ksebusunny@gmail.com>
2025-06-20 21:46:16 +0530

    updated jdk from 22-> 17 in pom.xml

---
+++ whatsapp-backend/src/main/java/com/jaruratcare/whatsapp/WhatsappBackendApplication.java
@@ -0,0 +1,11 @@
+package com.jaruratcare.whatsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsappBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WhatsappBackendApplication.class, args);
+    }
+}
